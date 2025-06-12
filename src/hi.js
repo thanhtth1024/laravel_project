6db6e4b162998269d90184b7fa33d116da33725e
Thanh <thanhtth1024@gmail.com>
2025-06-12 10:10:32 +0700

    commit

---
+++ src/hi.js
@@ -0,0 +1,12 @@
+<html lang="vi">
+<head>
+  <meta charset="UTF-8" />
+  <title>Demo JavaScript</title>
+</head>
+<body>
+  <button id="myButton">Bấm vào đây</button>
+  <p id="output"></p>
+
+  <script src="script.js"></script>
+</body>
+</html>
